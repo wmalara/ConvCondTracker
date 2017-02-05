76f0bf67a0cfbd11a1226a8845332bec53506185
Wojciech Malara <malara.wojciech@gmail.com>
2017-02-05 13:58:01 +0100

    Added Bootstrap references

--- src/ClientApp/src/main.js
+++ src/ClientApp/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import './vendor/bootstrap.scss';
 import App from './App.vue';
 import inputmask from './directives/inputmask';
 
